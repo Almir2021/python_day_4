db70da341b4093c6ad332c0f3e0eee533dd1fd2e
Almir2021 <bosnjicalmir@gmail.com>
2023-08-16 21:53:22 +0200

    Head or tails finished

---
+++ HeadOrTails.py
@@ -0,0 +1,11 @@
+# Remember to use the random module
+# Hint: Remember to import the random module here at the top of the file. 🎲
+import random
+# Write the rest of your code below this line 👇
+randomFloat = random.random()
+
+
+if randomFloat < 0.5:
+    print("Heads")
+else:
+    print("Tails")
