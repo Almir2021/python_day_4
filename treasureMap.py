03bd15f9a1d0fcdbb7bccdaf24ac40a049932dc2
Almir2021 <bosnjicalmir@gmail.com>
2023-08-17 16:26:55 +0200

    still working on this

---
+++ treasureMap.py
@@ -0,0 +1,29 @@
+# 🚨 Don't change the code below 👇
+row1 = ["⬜️", "️⬜️", "️⬜️"]
+row2 = ["⬜️", "⬜️", "️⬜️"]
+row3 = ["⬜️️", "⬜️️", "⬜️️"]
+map = [row1, row2, row3]
+print(f"{row1}\n{row2}\n{row3}")
+position = input("Where do you want to put the treasure? ")
+# 🚨 Don't change the code above 👆
+
+# Write your code below this row 👇
+
+output = []
+myStr = position
+print("The input string is:", myStr)
+for character in myStr:
+    output.append(character)
+print("The output is:", output)
+
+position1 = int(output[0])
+
+print(type(output[0]))
+
+map.insert(position, output[0])
+
+# Write your code above this row 👆
+
+# 🚨 Don't change the code below 👇
+print(f"{row1}\n{row2}\n{row3}")
+print(map)
