621fd027ccff50732deaa9459ae48f3ec9235f00
Almir2021 <bosnjicalmir@gmail.com>
2023-08-21 18:39:01 +0200

    Doing someting radical to get there

--- RockPaperScissors.py
+++ RockPaperScissors.py
@@ -1,3 +1,5 @@
+
+import random
 rock = '''
     _______
 ---'   ____)
@@ -26,3 +28,52 @@ scissors = '''
 '''
 
 # Write your code below this line 👇
+myChoice = " "
+print("What do you choose?")
+choice = input("Type R for Rock , P for paper or S for Scissors. ")
+
+if choice == "R":
+    print(rock)
+    myChoice = 0
+elif choice == "P":
+    print(paper)
+    myChoice = 1
+elif choice == "S":
+    print(scissors)
+    myChoice = 2
+
+print(myChoice)
+
+print("Computer chose:")
+
+computerChoice = [rock, paper, scissors]
+
+randomInt = random.randint(0, 2)
+print(computerChoice[randomInt])
+
+computerChosen = randomInt
+print(computerChosen)
+
+if myChoice == 0:
+    if myChoice == computerChosen:
+        print("It's draw")
+    elif myChoice == 1 and con:
+        print("You loose ")
+    elif myChoice == 2:
+        print("You won")
+
+if myChoice == 1:
+    if myChoice == computerChosen:
+        print("It's draw")
+    elif myChoice == 2:
+        print("You loose ")
+    elif myChoice == 0:
+        print("You won")
+
+if myChoice == 2:
+    if myChoice == computerChosen:
+        print("It's draw")
+    elif myChoice == 2:
+        print("You loose ")
+    elif myChoice == 1:
+        print("You won")
